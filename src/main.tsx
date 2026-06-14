39f6eddc18ed39bdc04f0464ea9431ac939b61d9
khushboo8252 <khushbooranjan8252@gmail.com>
2026-06-14 11:47:34 +0530

    fix language

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import './i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen'
-import './i18n'
 
 const queryClient = new QueryClient()
 
