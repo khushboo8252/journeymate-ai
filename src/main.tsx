ccc7dad401c96e10025fd9795989f86cc3e6d402
Nagendra Singh <Nagendra.Singh@leadsquared.com>
2026-06-14 20:56:06 +0530

    fix: enusre i18n is bundled in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import './i18n/index'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n/index'
 import { routeTree } from './routeTree.gen'
 
 const queryClient = new QueryClient()
@@ -17,8 +18,10 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </I18nextProvider>
   </StrictMode>
 )
